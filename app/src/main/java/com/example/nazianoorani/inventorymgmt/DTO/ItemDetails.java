a101816ed45b6842c033ab706b1150c082ae221c
Nazia Noorani <nazianoorani@Nazias-MacBook-Pro.local>
2016-01-12 13:50:50 +0530

    first commit

---
+++ app/src/main/java/com/example/nazianoorani/inventorymgmt/DTO/ItemDetails.java
@@ -0,0 +1,25 @@
+package com.example.nazianoorani.inventorymgmt.DTO;
+
+/**
+ * Created by nazianoorani on 23/12/15.
+ */
+public class ItemDetails {
+   private String itemName;
+   private int itemImage;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemImage() {
+        return itemImage;
+    }
+
+    public void setItemImage(int itemImage) {
+        this.itemImage = itemImage;
+    }
+}
